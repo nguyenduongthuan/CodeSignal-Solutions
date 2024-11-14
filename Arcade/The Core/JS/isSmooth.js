08594afe256a605921983fe554e39937748488ff
nguyenduongthuan75321@gmail.com <nguyenduongthuan75321@gmail.com>
2024-11-14 21:23:16 +0700

    11/14/2024

--- Arcade/The Core/JS/isSmooth.js
+++ Arcade/The Core/JS/isSmooth.js
@@ -1,8 +1,10 @@
 function solution(arr) {
-  let middle =
-    arr.length % 2
-      ? arr[Math.floor(arr.length / 2)]
-      : arr[arr.length / 2 - 1] + arr[arr.length / 2];
-  console.log(arr[arr.length - 1]);
-  return arr[0] === middle && middle === arr[arr.length - 1];
+  // let middle = arr.length % 2 ? arr[Math.floor(arr.length / 2)] : (arr[arr.length / 2 - 1] + arr[arr.length / 2])
+  return (
+    arr[0] ===
+      (arr.length % 2
+        ? arr[Math.floor(arr.length / 2)]
+        : arr[arr.length / 2 - 1] + arr[arr.length / 2]) &&
+    arr[0] === arr[arr.length - 1]
+  );
 }
